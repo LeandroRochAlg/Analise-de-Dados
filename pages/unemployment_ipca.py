bfcfaa1f6feff4a872ed1c180559303165dc9c89
LeandroRochAlg <leandrochaju@hotmail.com>
2024-07-02 19:28:50 -0300

    chore: Update unemployment and IPCA graphs

--- pages/unemployment_ipca.py
+++ pages/unemployment_ipca.py
@@ -2,7 +2,7 @@ from dash import dcc, html
 import plotly.graph_objs as go
 from data_processing import unemployment_data_filtered, ipca_filtered_data, pandemic_start, war_start
 
-unemployment_ipca_figure = {
+unemployment_figure = {
     'data': [
         go.Scatter(
             x=unemployment_data_filtered['Date'],
@@ -10,14 +10,6 @@ unemployment_ipca_figure = {
             mode='lines',
             name='Taxa de Desemprego',
             line=dict(color='purple')
-        ),
-        go.Scatter(
-            x=ipca_filtered_data['data'],
-            y=ipca_filtered_data['valor'],
-            mode='lines',
-            name='IPCA',
-            line=dict(color='orange'),
-            yaxis='y2'
         ),
         go.Scatter(
             x=[pandemic_start, pandemic_start],
@@ -37,17 +29,75 @@ unemployment_ipca_figure = {
     'layout': go.Layout(
         xaxis=dict(title='Date'),
         yaxis=dict(title='Taxa de Desemprego', titlefont=dict(color='purple'), tickfont=dict(color='purple')),
-        yaxis2=dict(title='IPCA', titlefont=dict(color='orange'), tickfont=dict(color='orange'), overlaying='y', side='right'),
         legend=dict(x=0, y=1.2),
-        margin=dict(l=50, r=50, t=50, b=50)
+        margin=dict(l=50, r=50, t=50, b=50),
+        annotations=[
+            dict(
+                x=0,
+                y=-0.11,
+                xref='paper',
+                yref='paper',
+                showarrow=False,
+                text=f'<a href="https://www.ibge.gov.br/estatisticas/sociais/trabalho/9173-pesquisa-nacional-por-amostra-de-domicilios-continua-trimestral.html?=&t=series-historicas&utm_source=landing&utm_medium=explica&utm_campaign=desemprego" target="_blank">Fonte: IBGE</a>',
+                font=dict(size=12),
+            )
+        ]
+    )
+}
+
+ipca_figure = {
+    'data': [
+        go.Scatter(
+            x=ipca_filtered_data['data'],
+            y=ipca_filtered_data['valor'],
+            mode='lines',
+            name='IPCA',
+            line=dict(color='orange')
+        ),
+            go.Scatter(
+            x=[pandemic_start, pandemic_start],
+            y=[ipca_filtered_data['valor'].min(), ipca_filtered_data['valor'].max()],
+            mode='lines',
+            name='Início da Pandemia',
+            line=dict(color='black', dash='dash')
+        ),
+        go.Scatter(
+            x=[war_start, war_start],
+            y=[ipca_filtered_data['valor'].min(), ipca_filtered_data['valor'].max()],
+            mode='lines',
+            name='Início da Guerra na Ucrânia',
+            line=dict(color='grey', dash='dash')
+        )
+    ],
+    'layout': go.Layout(
+        xaxis=dict(title='Date'),
+        yaxis=dict(title='IPCA', titlefont=dict(color='orange'), tickfont=dict(color='orange')),
+        legend=dict(x=0, y=1.2),
+        margin=dict(l=50, r=50, t=50, b=50),
+        annotations=[
+            dict(
+                x=0,
+                y=-0.11,
+                xref='paper',
+                yref='paper',
+                showarrow=False,
+                text=f'<a href="https://dadosabertos.bcb.gov.br/dataset/10841-indice-de-precos-ao-consumidor-amplo-ipca---bens-nao-duraveis" target="_blank">Fonte: Dados Abertos</a>',
+                font=dict(size=12),
+            )
+        ]
     )
 }
 
 layout = html.Div([
-    html.H2("Taxa de Desemprego e IPCA de bens não-duráveis", className='text-center my-4'),
+    html.H2("Taxa de Desemprego", className='text-center my-4'),
     dcc.Graph(
-        id='unemployment-ipca',
-        figure=unemployment_ipca_figure
+        id='unemployment',
+        figure=unemployment_figure
+    ),
+    html.H2("IPCA Bens Não-Duráveis", className='text-center my-4'),
+    dcc.Graph(
+        id='ipca',
+        figure=ipca_figure
     ),
     html.P("Aqui você pode visualizar a evolução da taxa de desemprego e do IPCA de bens não-duráveis ao longo do tempo."),
     html.P(["O ", html.Strong("IPCA"), " é o índice oficial de inflação do Brasil, calculado pelo IBGE. Ele mede a variação de preços de um conjunto de produtos e serviços consumidos pelas famílias brasileiras com rendimentos de 1 a 40 salários mínimos."]),
